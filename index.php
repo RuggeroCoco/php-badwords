a3a83dc539c261f62e7d2fdc0b0a5b5687ce6a4c
RuggeroCoco <ruggerococo94@gmail.com>
2023-05-03 17:47:29 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$username = "Ruggero";
+
+echo "Ciao" . $username;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+    <h1>Hello PHP</h1>
+    <h2>Ciao <?php echo $username; ?</h2>
+
+</body>
+</html>
